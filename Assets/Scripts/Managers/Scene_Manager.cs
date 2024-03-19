27bc2aec543c8af9bb3179c64cc2f11f3fc68c46
marconium <marcjc52@gmail.com>
2024-03-19 20:52:16 +0100

    .

--- Assets/Scripts/Managers/Scene_Manager.cs
+++ Assets/Scripts/Managers/Scene_Manager.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Scene_Manager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public void QuitAplication()
     {
-        
+        Application.Quit();
+    }
+    public void LoadScene(int value)
+    {
+        SceneManager.LoadScene(value);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
